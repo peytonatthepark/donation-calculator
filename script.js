89c9a01310d28e709c2390a0f3b53f1db55270be
Peyton Swan <peyton@church-at-the-park.org>
2025-07-31 13:08:22 -0700

    done

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const items = [
   { name: "Small Appliances", category: "Kitchen Essentials", cost: 2000 }
 ];
 
-// 🧼 Sanitize donation input to allow only digits
+// Sanitize donation input to allow only digits
 function sanitizeInput(input) {
   input.value = input.value.replace(/[^\d]/g, '');
 }
